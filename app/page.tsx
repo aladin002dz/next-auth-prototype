e48992d22c77564b35470e1ad71f1bdcb9713e4f
aladin002dz <mahfoudh.arous@gmail.com>
2025-02-11 18:02:17 +0100

    redirection after signIn and after signOut

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import SignIn from "@/components/sign-in";
-import SignOut from "@/components/sign-out";
-import { auth } from "@/auth";
 
 export default async function Home() {
-  const session = await auth()
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>{JSON.stringify(session)}</p>
+    <div className="min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex justify-center">
       <SignIn />
-      <SignOut />
       </main>
     </div>
   );
