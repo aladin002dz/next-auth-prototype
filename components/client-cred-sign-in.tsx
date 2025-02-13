9c3f06c6b43bf6078e3c89441058219eb5969268
aladin002dz <mahfoudh.arous@gmail.com>
2025-02-13 16:26:11 +0100

    fix: unused router constant

--- components/client-cred-sign-in.tsx
+++ components/client-cred-sign-in.tsx
@@ -1,12 +1,10 @@
 "use client"
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
-import { useRouter } from "next/navigation";
 
 export default function ClientCredentialsSignIn() {
     const [error, setError] = useState(null);
     const [data, setData] = useState({ email: 'aurore@domain.com', password: '12345678' });
-    const router = useRouter();
     const handleChange = (e) => {
         const { name, value } = e.target;
         setData(prevData => ({ ...prevData, [name]: value }));
@@ -43,12 +41,12 @@ export default function ClientCredentialsSignIn() {
                     <label className="block text-sm font-medium text-gray-700 mb-1">
                         Email
                     </label>
-                    <input 
-                        type="email" 
-                        name="email" 
-                        value={data.email} 
-                        onChange={handleChange} 
-                        required 
+                    <input
+                        type="email"
+                        name="email"
+                        value={data.email}
+                        onChange={handleChange}
+                        required
                         className="w-full rounded-lg border border-gray-300 px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-gray-300"
                     />
                 </div>
@@ -56,12 +54,12 @@ export default function ClientCredentialsSignIn() {
                     <label className="block text-sm font-medium text-gray-700 mb-1">
                         Password
                     </label>
-                    <input 
-                        type="password" 
-                        name="password" 
-                        value={data.password} 
-                        onChange={handleChange} 
-                        required 
+                    <input
+                        type="password"
+                        name="password"
+                        value={data.password}
+                        onChange={handleChange}
+                        required
                         className="w-full rounded-lg border border-gray-300 px-4 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-gray-300"
                     />
                 </div>
